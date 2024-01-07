76872972953e0f923f43ef07a0a5ffac631e6107
bzrudski <benjamin.rudski@mail.mcgill.ca>
2024-01-07 17:11:19 -0500

    fix: Add version information to the package init.

--- src/vectorose/__init__.py
+++ src/vectorose/__init__.py
@@ -2,3 +2,6 @@ from vectorose import vectorose
 from vectorose import main
 from vectorose import plotting
 from vectorose import vf_io
+
+from importlib_metadata import version
+__version__ = version("vectorose")
