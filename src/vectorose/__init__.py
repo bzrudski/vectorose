050f065ad0c3b71e26736a73f55ff887543476f7
bzrudski <benjamin.rudski@mail.mcgill.ca>
2024-04-15 14:19:51 -0400

    feat: add axis plotting to mesh-based histogram (buggy).

--- src/vectorose/__init__.py
+++ src/vectorose/__init__.py
@@ -2,6 +2,7 @@ from vectorose import vectorose
 from vectorose import main
 from vectorose import plotting
 from vectorose import vf_io
+from vectorose import triplot
 
 from importlib_metadata import version
 __version__ = version("vectorose")
