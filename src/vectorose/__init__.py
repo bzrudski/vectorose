73e2320ee7b7cb09e9ea88371baee1464cb08601
bzrudski <benjamin.rudski@mail.mcgill.ca>
2024-05-13 15:26:56 -0400

    refactor: add Tregenza to top-level init.

--- src/vectorose/__init__.py
+++ src/vectorose/__init__.py
@@ -3,6 +3,7 @@ from vectorose import main
 from vectorose import plotting
 from vectorose import vf_io
 from vectorose import triplot
+from vectorose import rectplot
 
 from importlib_metadata import version
 __version__ = version("vectorose")
