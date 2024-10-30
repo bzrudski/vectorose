2df78237dc1d81dae0020ee17e0a3f299b407f08
bzrudski <benjamin.rudski@mail.mcgill.ca>
2024-10-30 16:03:37 -0400

    refactor: import stats in the pacakge root.

--- src/vectorose/__init__.py
+++ src/vectorose/__init__.py
@@ -5,6 +5,7 @@ from vectorose import vf_io
 from vectorose import triangle_sphere
 from vectorose import tregenza_sphere
 from vectorose import util
+from vectorose import stats
 
 from importlib_metadata import version
 
