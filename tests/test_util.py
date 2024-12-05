ee9a51113181fc56d603a46512e135994109e3e2
bzrudski <49771453+bzrudski@users.noreply.github.com>
2024-12-05 13:12:41 -0500

    test: add unit tests for remaining utility functions and reformat code.

--- tests/test_util.py
+++ tests/test_util.py
@@ -6,13 +6,40 @@ module.
 
 
 import numpy as np
-import pytest
 import scipy
 
-from vectorose import util
+import vectorose as vr
 
 
 RANDOM_SEED = 20240818
+
+
+def test_magnitude_computation():
+    """Test the magnitude function.
+
+    Unit test for :func:`util.compute_vector_magnitudes`.
+    """
+
+    # Define two vectors
+    vectors = np.array([
+        [0, 1, 0],
+        [3, 4, 5]
+    ])
+
+    expected_magnitudes_3d = [1, 5 * np.sqrt(2)]
+    expected_magnitudes_2d = [1, 5]
+
+    magnitudes = vr.util.compute_vector_magnitudes(vectors)
+
+    # Check the 3D magnitudes
+    computed_magnitudes_3d = magnitudes[:, vr.util.MagnitudeType.THREE_DIMENSIONAL]
+
+    assert np.all(np.isclose(computed_magnitudes_3d, expected_magnitudes_3d))
+
+    # Check the in-plane magnitudes
+    computed_magnitudes_2d = magnitudes[:, vr.util.MagnitudeType.IN_PLANE]
+
+    assert np.all(np.isclose(computed_magnitudes_2d, expected_magnitudes_2d))
 
 
 def test_flatten_vector_field():
@@ -27,7 +54,7 @@ def test_flatten_vector_field():
     )
 
     # Flatten the vector field
-    my_flat_vector_field = util.flatten_vector_field(my_random_vector_field)
+    my_flat_vector_field = vr.util.flatten_vector_field(my_random_vector_field)
 
     # Test to make sure the dimensionality is correct
     assert my_flat_vector_field.ndim == 2
@@ -57,7 +84,7 @@ def test_remove_zero_vectors():
     )
 
     # Remove the zero vectors
-    non_zero_vectors_cleaned = util.remove_zero_vectors(my_combined_vectors)
+    non_zero_vectors_cleaned = vr.util.remove_zero_vectors(my_combined_vectors)
 
     # Check to make sure the two arrays have the same shape
     assert my_random_non_zero_vectors.shape == non_zero_vectors_cleaned.shape
@@ -80,7 +107,7 @@ def test_normalise_array_flat():
     my_random_array = np.random.default_rng(RANDOM_SEED).random(size=1000)
 
     # Normalise the array
-    normalised_array = util.normalise_array(my_random_array)
+    normalised_array = vr.util.normalise_array(my_random_array)
 
     # Now, check to see if the entries sum to one
     sum_of_entries = normalised_array.sum()
@@ -102,7 +129,7 @@ def test_normalise_array_2d():
     my_random_array = np.random.default_rng(RANDOM_SEED).random(size=(1000, 3))
 
     # Normalise the array
-    normalised_array = util.normalise_array(my_random_array, axis=-1)
+    normalised_array = vr.util.normalise_array(my_random_array, axis=-1)
 
     # Now, check to see if the entries sum to one
     sum_of_entries = normalised_array.sum(axis=-1)
@@ -132,7 +159,7 @@ def test_normalise_vectors_no_zero():
     )
 
     # Normalise the vectors
-    normalised_vectors, magnitudes = util.normalise_vectors(my_random_vectors)
+    normalised_vectors, magnitudes = vr.util.normalise_vectors(my_random_vectors)
 
     # Check the magnitudes of the normalised vectors
     normalised_vector_norms = np.linalg.norm(normalised_vectors, axis=-1)
@@ -153,14 +180,10 @@ def test_normalise_vectors_with_zeros():
     # Generate zero vectors
     my_zero_vectors = np.zeros_like(my_random_vectors)
 
-    my_total_vectors = np.concatenate(
-        [my_random_vectors, my_zero_vectors], axis=0
-    )
+    my_total_vectors = np.concatenate([my_random_vectors, my_zero_vectors], axis=0)
 
     # Normalise the vectors
-    normalised_vectors, magnitudes = util.normalise_vectors(
-        my_total_vectors
-    )
+    normalised_vectors, magnitudes = vr.util.normalise_vectors(my_total_vectors)
 
     # Check the magnitudes of the normalised vectors
     normalised_vector_norms = np.linalg.norm(normalised_vectors, axis=-1)
@@ -193,7 +216,9 @@ def test_generate_representative_unit_vectors_magnitudes():
     )
 
     # Generate the representative unit vectors
-    my_unit_vectors = util.generate_representative_unit_vectors(my_random_vectors, 3000)
+    my_unit_vectors = vr.util.generate_representative_unit_vectors(
+        my_random_vectors, 3000
+    )
 
     # Check the magnitudes
     magnitudes = np.linalg.norm(my_unit_vectors, axis=-1)
@@ -213,19 +238,23 @@ def test_convert_vectors_to_axes():
     # Ensure that there are vectors in the bottom hemisphere
     if not np.any(my_random_vectors[:, -1] < 0):
         indices_to_flip = np.random.default_rng(RANDOM_SEED).choice(
-            np.arange(1000), size=300, replace=False,
+            np.arange(1000),
+            size=300,
+            replace=False,
         )
 
         my_random_vectors[tuple(indices_to_flip)] *= -1
 
     # Convert the vectors to axes
-    my_axes = util.convert_vectors_to_axes(my_random_vectors)
+    my_axes = vr.util.convert_vectors_to_axes(my_random_vectors)
 
     # Check to make sure the shape is the same
     assert my_axes.shape == my_random_vectors.shape
 
     # Check to make sure the norms are the same
-    assert np.all(np.linalg.norm(my_axes, axis=-1) == np.linalg.norm(my_random_vectors, axis=-1))
+    assert np.all(
+        np.linalg.norm(my_axes, axis=-1) == np.linalg.norm(my_random_vectors, axis=-1)
+    )
 
     # Check to make sure that all the z-coordinates are non-negative
     assert np.all(my_axes[:, -1] >= 0)
@@ -245,7 +274,7 @@ def test_create_symmetric_vectors_from_axes():
     )
 
     # Convert these axes into vectors
-    my_vectors = util.create_symmetric_vectors_from_axes(my_random_axes)
+    my_vectors = vr.util.create_symmetric_vectors_from_axes(my_random_axes)
 
     # Check the shape of the output
     n_axes, d_axes = my_random_axes.shape
@@ -255,7 +284,7 @@ def test_create_symmetric_vectors_from_axes():
 
     # Check to see that the order is preserved
     assert np.all(my_vectors[:n_axes] == my_random_axes)
-    assert np.all(my_vectors[n_axes:] == - my_random_axes)
+    assert np.all(my_vectors[n_axes:] == -my_random_axes)
 
 
 def test_convert_spherical_to_cartesian_coordinates():
@@ -265,26 +294,16 @@ def test_convert_spherical_to_cartesian_coordinates():
     """
 
     # Create some simple angles
-    input_angles = np.radians([
-        [0, 0],
-        [180, 0],
-        [90, 0],
-        [90, 180],
-        [90, 90],
-        [90, 270]
-    ])
+    input_angles = np.radians(
+        [[0, 0], [180, 0], [90, 0], [90, 180], [90, 90], [90, 270]]
+    )
 
-    expected_output = np.array([
-        [0, 0, 1],
-        [0, 0, -1],
-        [0, 1, 0],
-        [0, -1, 0],
-        [1, 0, 0],
-        [-1, 0, 0]
-    ])
+    expected_output = np.array(
+        [[0, 0, 1], [0, 0, -1], [0, 1, 0], [0, -1, 0], [1, 0, 0], [-1, 0, 0]]
+    )
 
     # Convert the angles to Cartesian coordinates
-    my_cartesian_coordinates = util.convert_spherical_to_cartesian_coordinates(
+    my_cartesian_coordinates = vr.util.convert_spherical_to_cartesian_coordinates(
         input_angles, radius=1
     )
 
@@ -299,28 +318,16 @@ def test_compute_vector_orientation_angles():
     """
 
     # Create the test input vectors and output angles
-    input_vectors = np.array([
-        [0, 0, 1],
-        [0, 0, -1],
-        [0, 1, 0],
-        [0, -1, 0],
-        [1, 0, 0],
-        [-1, 0, 0]
-    ])
+    input_vectors = np.array(
+        [[0, 0, 1], [0, 0, -1], [0, 1, 0], [0, -1, 0], [1, 0, 0], [-1, 0, 0]]
+    )
 
-    expected_output = np.radians([
-        [0, 0],
-        [180, 0],
-        [90, 0],
-        [90, 180],
-        [90, 90],
-        [90, 270]
-    ])
+    expected_output = np.radians(
+        [[0, 0], [180, 0], [90, 0], [90, 180], [90, 90], [90, 270]]
+    )
 
     # Run the conversion
-    computed_spherical_coords = util.compute_vector_orientation_angles(
-        input_vectors
-    )
+    computed_spherical_coords = vr.util.compute_vector_orientation_angles(input_vectors)
 
     # Check the output
     assert np.all(computed_spherical_coords == expected_output)
@@ -333,34 +340,23 @@ def test_convert_to_math_spherical_coordinates():
     """
 
     # Define our input angles
-    input_angles = np.radians([
-        [0, 0],
-        [180, 0],
-        [90, 0],
-        [45, 45],
-        [90, 180],
-        [90, 90],
-        [90, 270]
-    ])
+    input_angles = np.radians(
+        [[0, 0], [180, 0], [90, 0], [45, 45], [90, 180], [90, 90], [90, 270]]
+    )
 
     # Define the expected outputs
-    expected_outputs = np.radians([
-        [90, 0],
-        [90, 180],
-        [90, 90],
-        [45, 45],
-        [270, 90],
-        [0, 90],
-        [180, 90]
-    ])
+    expected_outputs = np.radians(
+        [[90, 0], [90, 180], [90, 90], [45, 45], [270, 90], [0, 90], [180, 90]]
+    )
 
     # And now perform the conversion
-    converted_angles = util.convert_to_math_spherical_coordinates(
+    converted_angles = vr.util.convert_to_math_spherical_coordinates(
         input_angles, use_degrees=False
     )
 
     # And check the equality
     assert np.all(converted_angles == expected_outputs)
+
 
 def test_convert_to_vectorose_spherical_coordinates():
     """Test conversion from FLE system to the custom spherical coordinates.
@@ -370,31 +366,88 @@ def test_convert_to_vectorose_spherical_coordinates():
     """
 
     # Define our input angles
-    input_angles = np.radians([
-        [90, 0],
-        [90, 180],
-        [90, 90],
-        [45, 45],
-        [270, 90],
-        [0, 90],
-        [180, 90]
-    ])
+    input_angles = np.radians(
+        [[90, 0], [90, 180], [90, 90], [45, 45], [270, 90], [0, 90], [180, 90]]
+    )
 
     # Define the expected outputs
-    expected_outputs = np.radians([
-        [0, 0],
-        [180, 0],
-        [90, 0],
-        [45, 45],
-        [90, 180],
-        [90, 90],
-        [90, 270]
-    ])
+    expected_outputs = np.radians(
+        [[0, 0], [180, 0], [90, 0], [45, 45], [90, 180], [90, 90], [90, 270]]
+    )
 
     # And now perform the conversion
-    converted_angles = util.convert_math_spherical_coordinates_to_vr_coordinates(
+    converted_angles = vr.util.convert_math_spherical_coordinates_to_vr_coordinates(
         input_angles, use_degrees=False
     )
 
     # And check the equality
     assert np.all(converted_angles == expected_outputs)
+
+
+def test_rotate_vectors_no_change():
+    """Test vector rotation for no change.
+
+    Unit test for :func:`util.rotate_vectors`.
+    """
+
+    # Create a set of vectors
+    vectors = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+    # Rotate them so that the pole is now on the bottom.
+    new_pole = np.array([0, 0, 1])
+
+    rotated_vectors = vr.util.rotate_vectors(vectors, new_pole)
+
+    # Now, we would expect the following result.
+    expected_result = vectors
+
+    # And now check the equality
+    assert np.all(np.isclose(rotated_vectors, expected_result))
+
+
+def test_rotate_vectors_flip():
+    """Test vector rotation for complete flip.
+
+    Unit test for :func:`util.rotate_vectors`.
+    """
+
+    # Create a set of vectors
+    vectors = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+    # Rotate them so that the pole is now on the bottom.
+    new_pole = np.array([0, 0, -1])
+
+    rotated_vectors = vr.util.rotate_vectors(vectors, new_pole)
+
+    # Now, we would expect the following result.
+    expected_result = np.array([[1, 0, 0], [0, -1, 0], [0, 0, -1]])
+
+    # And now check the equality
+    assert np.all(np.isclose(rotated_vectors, expected_result))
+
+
+def test_rotate_vectors_partial_rotation():
+    """Test vector rotation for partial rotation.
+
+    Unit test for :func:`util.rotate_vectors`.
+    """
+
+    # Create a set of vectors
+    vectors = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+    # Rotate them so that the pole is now on the bottom.
+    new_pole = np.array([1 / np.sqrt(2), 1 / np.sqrt(2), 0])
+
+    rotated_vectors = vr.util.rotate_vectors(vectors, new_pole)
+
+    # Now, we would expect the following result.
+    expected_result = np.array(
+        [
+            [1 / np.sqrt(2), -1 / np.sqrt(2), 0],
+            [0, 0, -1],
+            [1 / np.sqrt(2), 1 / np.sqrt(2), 0],
+        ]
+    )
+
+    # And now check the equality
+    assert np.all(np.isclose(rotated_vectors, expected_result))
