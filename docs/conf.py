d2b254b52e47c64f7aa83b199c3586eb825909f6
bzrudski <49771453+bzrudski@users.noreply.github.com>
2024-12-25 15:01:06 -0500

    docs: add debug option for autoapi.

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,8 @@ autoapi_options = [
     "special-members",
 ]
 
+autoapi_keep_files = False
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
